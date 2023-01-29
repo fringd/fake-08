a775f2886f7ac8045607d2570d418d517633af83
Ram Dobson <fringd@gmail.com>
2023-01-29 18:08:57 -0500

    Revert "mak wavs"
    This reverts commit 86f445043e52fb45b9f91bc5e8692c069b964f18.

--- test/audiotests.cpp
+++ test/audiotests.cpp
@@ -1,56 +1,7 @@
 #include "doctest.h"
 #include "../source/Audio.h"
 #include "../source/PicoRam.h"
-#include "../source/pinknoise.h"
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-
-typedef struct WAV_HEADER {
-  /* RIFF Chunk Descriptor */
-  uint8_t RIFF[4] = {'R', 'I', 'F', 'F'}; // RIFF Header Magic header
-  uint32_t ChunkSize;                     // RIFF Chunk Size
-  uint8_t WAVE[4] = {'W', 'A', 'V', 'E'}; // WAVE Header
-  /* "fmt" sub-chunk */
-  uint8_t fmt[4] = {'f', 'm', 't', ' '}; // FMT header
-  uint32_t Subchunk1Size = 16;           // Size of the fmt chunk
-  uint16_t AudioFormat = 1; // Audio format 1=PCM,6=mulaw,7=alaw,     257=IBM
-                            // Mu-Law, 258=IBM A-Law, 259=ADPCM
-  uint16_t NumOfChan = 1;   // Number of channels 1=Mono 2=Sterio
-  uint32_t SamplesPerSec = 22050;   // Sampling Frequency in Hz
-  uint32_t bytesPerSec = 22050 * 2; // bytes per second
-  uint16_t blockAlign = 2;          // 2=16-bit mono, 4=16-bit stereo
-  uint16_t bitsPerSample = 16;      // Number of bits per sample
-  /* "data" sub-chunk */
-  uint8_t Subchunk2ID[4] = {'d', 'a', 't', 'a'}; // "data"  string
-  uint32_t Subchunk2Size;                        // Sampled data length
-} wav_hdr;
-
-void write_wav(std::istream *in, const char *output_filename) {
-  static_assert(sizeof(wav_hdr) == 44, "");
-
-  std::string in_name = "test.bin"; // raw pcm data without wave header
-
-  uint32_t fsize = in->tellg();
-  in->seekg(0, std::ios::end);
-  fsize = (uint32_t)in->tellg() - fsize;
-  in->seekg(0, std::ios::beg);
-
-  wav_hdr wav;
-  wav.ChunkSize = fsize + sizeof(wav_hdr) - 8;
-  wav.Subchunk2Size = fsize + sizeof(wav_hdr) - 44;
-
-  std::ofstream out(output_filename, std::ios::binary);
-  out.write(reinterpret_cast<const char *>(&wav), sizeof(wav));
-
-  int16_t d;
-  for (int i = 0; i < fsize; ++i) {
-    // TODO: read/write in blocks
-    in->read(reinterpret_cast<char *>(&d), sizeof(int16_t));
-    out.write(reinterpret_cast<char *>(&d), sizeof(int16_t));
-  }
-}
 
 TEST_CASE("audio class behaves as expected") {
     //general setup
